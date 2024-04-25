6a5df826c668a274ecf0e79fe6df4214c4012ed8
Chris Maunder <chris.maunder@codeproject.com>
2024-04-24 22:19:33 -0400

    Comments

--- install.bat
+++ install.bat
@@ -2,10 +2,10 @@
 ::
 ::                         ObjectDetection (Coral)
 ::
-:: This script is only called from ..\..\src\setup.bat
+:: This script is only called from ..\..\CodeProject.AI-Server\src\setup.bat
 ::
 :: For help with install scripts, notes on variables and methods available, tips,
-:: and explanations, see /src/modules/install_script_help.md
+:: and explanations, see CodeProject.AI-Server\src\modules\install_script_help.md
 
 @if "%1" NEQ "install" (
     echo This script is only called from ..\..\src\setup.bat
--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@
 # The setup.sh script will find this install.sh file and execute it.
 #
 # For help with install scripts, notes on variables and methods available, tips,
-# and explanations, see /src/modules/install_script_help.md
+# and explanations, see CodeProject.AI-Server\src\modules\install_script_help.md
 
 if [ "$1" != "install" ]; then
     read -t 3 -p "This script is only called from: bash ../../setup.sh"
