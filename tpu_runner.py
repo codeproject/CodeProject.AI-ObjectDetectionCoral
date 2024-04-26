eaaa5483fddc68bbfcc5e0df0dcd4a1207b98848
Seth Price <seth.pricepages@gmail.com>
2024-04-26 14:04:57 -0700

    Fix some watchdog logging

--- tpu_runner.py
+++ tpu_runner.py
@@ -534,7 +534,7 @@ class TPURunner(object):
         while not self.watchdog_shutdown:
             if self.pipe and self.pipe.first_name is not None and \
                 time.time() - self.watchdog_time > self.max_idle_secs_before_recycle:
-                logging.warning("No work in {} seconds, watchdog shutting down TPUs.".format(self.max_idle_secs_before_recycle))
+                logging.info("No work in {} seconds, watchdog shutting down TPUs.".format(self.max_idle_secs_before_recycle))
                 self.runner_lock.acquire(timeout=MAX_WAIT_TIME)
                 self.pipe.delete()
                 self.runner_lock.release()
